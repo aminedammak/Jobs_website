8ce893700659a8fa82264a27cb42ddb36e5f3927
aminedammak <amine.dammak@softtodo.com>
2019-11-15 11:49:32 +0100

    Created the main navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
+import Navigation from "./Navigation";
 
 export default function header() {
     return (
         <div className="header">
             <Container>
-                <Row>
-                    <Col sm={2}>Logo</Col>
-                    <Col sm={6}>Navigation</Col>
-                    <Col sm={{span:2, offset: 2}}>login logout</Col>
-                </Row>
+                <Navigation />
             </Container>
         </div>
     )
---
+++ src/components/Header/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+
+export default function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home"><h1>Jobs</h1></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavDropdown title="For candidates" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Browse Jobs</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Browse categories</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Browser companies</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="For employers" id="basic-nav-dropdown-2">
+                        <NavDropdown.Item href="#action/3.1">Browse candidates</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Post a job</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Employer dashboard</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <div className="logging-btn">
+                    <Button variant="secondary" className="m-2">Sign Up</Button>
+                    <Button variant="secondary" className="my-2">Log In</Button>
+                </div>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
